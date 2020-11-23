3c4d635c9f9477816612e1ae096f1bd4f71c5b64
Chris <chrisatmachine@gmail.com>
2020-11-22 22:09:29 -0500

    update

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     name: str
-    prin: float
+    price: float
     is_offer: Optional[bool] = None
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-# TODO create update item endpoint
-# @app.put("/items")
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
